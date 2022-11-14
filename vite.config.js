ecfe9fc858397b82d9ebf086c2c10ff24d977c1d
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-14 04:32:27 +0100

    config modifications

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 8001,
+    strictPort: true,
+    open: true,
+    https: false,
+    proxy : {
+      '/api': {
+        target: 'http://localhost:8000'
+      },
+      '/user': {
+        target: 'http://localhost:8080'
+      }
+    }
+  }
 })
