2ad5794b73a00d913f208ed8203e344a898258fd
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-23 03:26:57 +0100

    added hook for form input

--- src/services/Hooks.jsx
+++ src/services/Hooks.jsx
@@ -4,3 +4,17 @@ import { LogContext } from "./log/Contexts";
 
 export const useLogs = () => useContext(LogContext);
 
+export const useFormInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    const inputProps = {
+        value: value,
+        onChange: handleChange
+    };
+
+    return inputProps;
+}
